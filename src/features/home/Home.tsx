98a96ff237b930c42e078bbf50b31ed286bc80bc
sadyanis <126914554+sadyanis@users.noreply.github.com>
2025-10-08 21:55:02 +0200

    feature/correction carousel

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,6 +1,4 @@
 import Header from "../../global_components/Header";
-
-import Footer from "../../global_components/Footer";
 import Overview from "./components/Overview";
 // import { Outlet } from 'react-router-dom'
 function Home() {
